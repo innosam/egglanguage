8fd5b0c08804a116fe5835c53dd1f2548f6ff43d
Harkirat Singh <hsingh@microsoft.com>
2016-04-17 01:13:30 +0530

    implemented eval for egg
    - eval function
    - operators
    - inbuilt function : do, if, define

--- interpret.js
+++ interpret.js
@@ -8,7 +8,7 @@ if(process.argv.length != 3)
 
 var fileContent = fs.readFileSync( process.argv[2],"utf8");
 
-function match_the_string(input)
+function parse_egg(input)
 {
 
    var token = {};
@@ -48,7 +48,7 @@ function match_the_string(input)
    input = input.slice(match[0].length);
    while(!(match = /^\s*\)/.exec(input)))
    {
-     var result = match_the_string(input);
+     var result = parse_egg(input);
      expression.args.push(result.exp);
      input = result.rest;
      if(match = /^\s*,/.exec(input))
@@ -61,8 +61,104 @@ function match_the_string(input)
    
    return {exp:expression, rest:input};
 }
+var ast = parse_egg(fileContent).exp;
 
 
 
-var output = JSON.stringify(match_the_string(fileContent).exp);
-console.log(output);
+function evals(ast, env)
+{
+  if(ast.type == "apply")
+  {
+    return env[ast.operator["name"]](ast.args, env);
+  }
+  else if(ast.type == "word")
+  {
+    return env[ast.name];
+  }
+  else if(ast.type == "value")
+  {
+    return ast.value;
+  }
+  console.log(ast);
+  throw new Error("Unexpected expression " + ast.type);
+}
+
+var builtIn = {};
+
+builtIn["do"] = function(args, env)
+{
+  var argn = args.length;
+  var result = null;
+  for( var i=0; i<(argn) ; i++ )
+  {
+    result = evals(args[i], env);
+  }
+  return result;
+}
+
+builtIn["define"] = function(args, env)
+{
+  if(args.length != 2 || args[0].type != "word")
+  {
+    throw Error("Number of arguements for define should be two");
+  }
+  
+  var result  = evals(args[1],env) 
+  env[args[0].name] = result;
+  return result;
+}
+
+builtIn["if"] = function(args, env)
+{
+  if(args.length != 3)
+  {
+    throw Error("Number of arguements for if should be three");
+  }
+ 
+  var result  = evals(args[0],env) 
+  if (result == true)
+  {
+     return evals(args[1], env);
+  }
+  if (result == false)
+  {
+     return evals(args[2], env);
+  }
+
+  throw new Error("first arguement should be a boolean value");
+}
+
+var ops= ["+", "-", "*", "/", "==", "<", ">"]
+ops.forEach(function(op) 
+{
+  builtIn[op] = function(args, env) 
+  {
+    if(args.length != 2)
+    {
+      throw new Error("Expected only two arguments");
+    }
+
+    var op_func = new Function("a, b",  "return a " + op + " b;");
+    return op_func(evals(args[0], env), evals(args[1], env));    
+  }
+});
+
+
+builtIn["print"] = function(args, env)
+{
+  if(!(args.length >= 1))
+  {
+    throw Error("Number of arguement should be atleast one");
+  }
+ 
+  var argn = args.length;
+  for(var i = 0; i < argn ; i++)
+  {
+    console.log(evals(args[i], env));
+  }
+}
+
+var globals = Object.create(builtIn);
+
+evals(ast, globals);
+
